c3d72a098acc2027712222409f7322884f296536
Teepluss <admin@jquerytips.com>
2014-02-21 15:08:59 +0700

    curl instead file_get_content

--- src/Teepluss/Up/Uploader.php
+++ src/Teepluss/Up/Uploader.php
@@ -271,7 +271,19 @@ class Uploader {
         $filename = $this->name($url);
 
         // Get file binary.
-        $bin = file_get_contents($url);
+        $ch = curl_init();
+
+        curl_setopt ($ch, CURLOPT_URL, $url);
+        curl_setopt ($ch, CURLOPT_HEADER, 0);
+        curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt ($ch,CURLOPT_CONNECTTIMEOUT,120);
+        curl_setopt ($ch,CURLOPT_TIMEOUT,120);
+
+        // Response returned.
+        $bin = curl_exec($ch);
+
+        curl_close($ch);
 
         // Path to write file.
         $uploadPath = $path.$filename;
